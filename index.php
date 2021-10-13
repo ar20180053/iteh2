9081df7a7086cf21bc1cd694eb84f83fdc36cc05
ar20180053 <andjelija.randjelovic99@gmail.com>
2021-10-13 15:59:14 +0200

    nadjena greska

--- index.php
+++ index.php
@@ -1,18 +1,46 @@
+<?php
+require "model/dbbroker.php";
+require "model/user.php";
+session_start();
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+    $conn = new mysqli();
+
+    $korisnik = new User(null, $uname, $upass);
+    //staticka f-ja preko klase -> $odg = User :: logInUser($uname,$upass,$conn);
+    $odgovor = $korisnik->logInUser($uname, $upass, $conn);
+    if ($odgovor->num_rows == 1) {
+        echo "Uspesno ste se prijavili";
+    }
+    $_SESSION['user_id'] = $korisnik->id;
+    header('Location: home.php');
+    exit();
+} else {
+    echo "Niste se prijavili";
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>FON: Zakazivanje kolokvijuma</title>
 
 </head>
+
 <body>
     <div class="login-form">
         <div class="main-div">
             <form method="POST" action="#">
                 <div class="container">
                     <label class="username">Korisnicko ime</label>
-                    <input type="text" name="username" class="form-control"  required>
+                    <input type="text" name="username" class="form-control" required>
                     <br>
                     <label for="password">Lozinka</label>
                     <input type="password" name="password" class="form-control" required>
@@ -22,7 +50,8 @@
             </form>
         </div>
 
-        
+
     </div>
 </body>
+
 </html>
